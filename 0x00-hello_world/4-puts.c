b69419180518331908456cb41041bf18e1891e3b
EddV23 <edjevan40@gmail.com>
2023-08-10 17:50:02 +0000

    prints an output using function puts not printf

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints an exact output
+ * using the funtion puts
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
