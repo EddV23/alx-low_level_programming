00d5c43c1a4eee5d1b003aaeb0a7aaedfc260fef
EddV23 <edjevan40@gmail.com>
2023-08-16 02:36:19 +0000

    prints first 50 Fibonacci numbers, starting with 1 and 2, followed by a new line

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 50 fibonacci numbers, starting
+ *        with 1 and 2, followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/*declaration of variables*/
+	int a = 1, b = 2, temp, n;
+
+	printf("%d, %d", a, b);
+	for (int i = 2; i < 50; i++)
+	{
+		temp = a + b;
+		printf(", %d", temp);
+		a = b;
+		b = temp;
+	}
+	printf("\n");
+	return (0);
+}
