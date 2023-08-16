b1595990122bbec8222d8db75e8bb182d414c9c4
EddV23 <edjevan40@gmail.com>
2023-08-16 02:58:50 +0000

    prints the sum of even-valued Fibonacci terms not exceeding 4,000,000

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - prints the sum of the even-valued terms in the
+ *        Fibonacci sequence that do not exceed 4,000,000,
+ *        followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/*declaration of variables*/
+	long a = 1, b = 2, temp, n;
+
+	while (a <= 4000000)
+	{
+		if (a % 2 == 0)
+			sum += a;
+		temp = a + b;
+		a = b;
+		b = temp;
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
