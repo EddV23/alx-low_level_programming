730f2494f8a7a74cd874f117c59f72fcd8619486
EddV23 <edjevan40@gmail.com>
2023-08-16 03:11:40 +0000

    prints the first 98 Fibonacci numbers, starting with 1 and 2, followed by a new line

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 50 fibonacci numbers, starting
+ *        with 1 and 2, followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/*declaration of variables*/
+	long a = 1, b = 2, temp, n;
+
+	printf("%ld, %ld", a, b);
+	for (n = 2; n < 98; n++)
+	{
+		temp = a + b;
+		printf(", %ld", temp);
+		a = b;
+		b = temp;
+	}
+	printf("\n");
+	return (0);
+}
