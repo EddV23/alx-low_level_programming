18828d7ccd9be6f1a8dec514321f17152adc41c2
EddV23 <edjevan40@gmail.com>
2023-08-17 22:05:54 +0000

    prints a triangle with the character # and given size, using _putchar

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle followed by
+ *                  a new line, using _putchar
+ * @size : the size of the triangle
+ * Return: void
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size - i - 1; j++)
+				_putchar(' ');
+			for (j = 0; j <= i; j++)
+				_putchar ('#');
+			_putchar('\n');
+		}
+	}
+}
