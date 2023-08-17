9a43ab20012a0182aaf73637a77d78b25b724909
EddV23 <edjevan40@gmail.com>
2023-08-17 21:15:06 +0000

    prints the numbers from 1 to 100, except replaces multiples of three and five with Fizz, Buzz, and FizzBuzz where specified

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * main - prints the numbers from 0 to 100 followed by
+ *        a new line, but for multiples of three prints Fizz
+ *        instead of the number, for multiples of five
+ *        prints Buzz, and FizzBuzz for numbers which are
+ *        three and five
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 1; num <= 100 ; num++)
+	{
+		if (num % 3 == 0)
+			printf("Fizz ");
+		else if (num % 5 == 0)
+			printf("Buzz ");
+		else if (num % 3 == 0 && num % 5 == 0)
+			printf("FizzBuzz ");
+		else
+			printf("%d ", num);
+	}
+	printf("\n");
+	return (0);
+}
