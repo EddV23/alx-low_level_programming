abb050a75f78ad3a963720e43b6e9da360fa8849
EddV23 <edjevan40@gmail.com>
2023-08-21 23:16:55 +0000

    prints the second half of a string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,11 +11,6 @@ void puts_half(char *str)
 	int i = 0;
 	int half = 0;
 
-	/**
-	*for (i = 0; str[i] != '\0'; i++)
-	*	_putchar(str[i]);
-	*	i += 2;
-	*/
 	while (str[i] != '\0')
 		i++;
 	half = i / 2;
