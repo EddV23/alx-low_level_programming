baf31843f4ab906ed4331eeacb8d84517d7a8240
EddV23 <edjevan40@gmail.com>
2023-08-24 02:40:00 +0000

    prints an integer, using _putchar

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,28 +8,12 @@
  */
 void print_number(int n)
 {
-	int divisor = 1;
-	int num = n;
-
-	if (n == 0)
-	{
-		_putchar('0');
-		return;
-	}
 	if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
 	}
-	while (num / 10 != 0)
-	{
-		divisor *= 10;
-		num /= 10;
-	}
-	while (divisor != 0)
-	{
-		_putchar((n / divisor) + '0');
-		n %= divisor;
-		divisor /= 10;
-	}
+	if (n / 10 != 0)
+		print_number(n / 10);
+	_putchar(n % 10 + '0');
 }
