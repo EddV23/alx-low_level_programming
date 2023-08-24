16c413aa94a69b51bc28243b6c79c1b2ed49d451
EddV23 <edjevan40@gmail.com>
2023-08-24 18:17:38 +0000

    add two numbers stored as strings

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -12,32 +12,31 @@
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int carry = 0;
-	int index_r = size_r - 1;
-	int index_n1 = 0;
-	int index_n2 = 0;
+	int idx_r = size_r - 1;
+	int idx_n1 = 0;
+	int idx_n2 = 0;
 	int i = 0, digit = 0;
 
-	r[index_r] = '\0';
-	while (index_r > 0 || carry)
+	r[idx_r] = '\0';
+	while (idx_r > 0 || carry)
 	{
-		int num1 = (index_n1 < size_r - 1 && n1[index_n1] != '\0') ? n1[index_n1] - '0' : 0;
-		int num2 = (index_n2 < size_r - 1 && n2[index_n2] != '\0') ? n2[index_n2] - '0' : 0;
-		int sum = num1 + num2;
+		int n_1 = (idx_n1 < size_r - 1 && n1[idx_n1] != '\0') ? n1[idx_n1] - '0' : 0;
+		int n_2 = (idx_n2 < size_r - 1 && n2[idx_n2] != '\0') ? n2[idx_n2] - '0' : 0;
+		int sum = n_1 + n_2;
 
-		
 		carry = sum / 10;
 		digit = sum % 10;
-		if (index_r >= 0)
-			r[index_r] = digit + '0';
-		index_r--;
-		index_n1++;
-		index_n2++;
+		if (idx_r >= 0)
+			r[idx_r] = digit + '0';
+		idx_r--;
+		idx_n1++;
+		idx_n2++;
 	}
-	if (index_r < 0)
+	if (idx_r < 0)
 		return (0);
-	for (i = 0; i < size_r - index_r - 1; i++)
+	for (i = 0; i < size_r - idx_r - 1; i++)
 	{
-		r[i] = r[index_r + i];
+		r[i] = r[idx_r + i];
 	}
 	return (r);
 }
