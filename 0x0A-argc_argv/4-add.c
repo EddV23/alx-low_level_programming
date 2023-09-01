0d3c23ac152637bc1da994d37e5714c0a3b5718d
EddV23 <edjevan40@gmail.com>
2023-09-01 18:27:48 +0000

    adds positive numbers provided as command-line arguments and prints the result

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -14,27 +14,19 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, num, result = 0;
+	int i, num, result;
 
-	if (argc == 1)
-	{
-		printf("0\n");
-		return (0);
-	}
 	for (i = 1; i < argc; i++)
 	{
-		num = atoi(argv[i]);
-		if (num == 0 && argv[i][0] != '0')
+		for (num = 0; argv[i][num] != '\0'; num++)
 		{
-			printf("Error\n");
-			return (1);
+			if (!isdigit(argv[i][num]))
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
-		else if (num < 0)
-		{
-			printf("Error\n");
-			return (1);
-		}
-		result += num;
+		result += atoi(argv[i]);
 	}
 	printf("%d\n", result);
 	return (0);
