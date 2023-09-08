4a6b3a0074e2dbe06cc2638d73f7a8080d5f4102
EddV23 <edjevan40@gmail.com>
2023-09-08 01:09:19 +0000

    multiplies two positive numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -17,7 +17,7 @@ int main(int argc, char *argv[])
 {
 	char *num1, *num2;
 	int length1, length2, totalLen, *res, tempSum;
-	int i = 0, nz = 0, d1, d2;
+	int i, nz = 0, d1, d2;
 
 	num1 = argv[1];
 	num2 = argv[2];
