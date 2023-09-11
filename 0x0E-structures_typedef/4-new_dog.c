ac341824afa9bc7c96e7251e8d967a4c22edbbb4
EddV23 <edjevan40@gmail.com>
2023-09-11 22:46:19 +0000

    creates a new dog and stores a copy of name and owner

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,7 +1,35 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "dog.h"
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory,
+ *           which contains a copy of the string given as a parameter
+ * @str: the string to duplicate.
+ *
+ * Return: A pointer to the duplicated string, or
+ *         NULL if str is NULL or if memory allocation fails
+ */
+char *_strdup(char *str)
+{
+	char *duplicate;
+	unsigned int i, length = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[length] != '\0')
+		length++;
+
+	duplicate = malloc(sizeof(char) * (length + 1));
+
+	if (duplicate == NULL)
+		return (NULL);
+
+	for (i = 0; i <= length; i++)
+		duplicate[i] = str[i];
+
+	return (duplicate);
+}
 
 /**
  * new_dog - creates a new dog and stores a copy of name and owner
@@ -19,25 +47,17 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_dog_ptr = malloc(sizeof(dog_t));
 	if (new_dog_ptr == NULL)
 		return (NULL);
-
-	name_copy = strdup(name);
-	if (name_copy == NULL)
-	{
-		free(new_dog_ptr);
-		return (NULL);
-	}
-
-	owner_copy = strdup(owner);
-	if (owner_copy == NULL)
+	name_copy = _strdup(name);
+	owner_copy = _strdup(owner);
+	if (name_copy == NULL || owner_copy == NULL)
 	{
 		free(name_copy);
+		free(owner_copy);
 		free(new_dog_ptr);
 		return (NULL);
 	}
-
 	new_dog_ptr->name = name_copy;
 	new_dog_ptr->age = age;
 	new_dog_ptr->owner = owner_copy;
-
 	return (new_dog_ptr);
 }
