a864eab1d494d43263f69e48bab82b743878e965
EddV23 <edjevan40@gmail.com>
2023-09-22 15:11:54 +0000

    a 64-bit program in assembly that prints Hello, Holberton, followed by a new line

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,6 +1,6 @@
 section .data
 	message: db "Hello, Holberton", 0
-	format:	 db "%s", 10, 0
+	format:	db "%s", 10, 0
 
 section .text
 	global main
@@ -9,10 +9,10 @@ extern printf
 
 main:
 	push rbp
-	mov rdi, format
-	mov rsi, message
+	mov rdi,format
+	mov rsi,message
+	mov rax,0
 	call printf
 	pop rbp
-	mov rax, 0x60
-	xor rdi, rdi
+	mov rax,0
 	ret
