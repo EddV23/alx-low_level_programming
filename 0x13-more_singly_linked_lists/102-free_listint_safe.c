fb5bf203c007dc83284a088fa13b8d0543d97944
EddV23 <edjevan40@gmail.com>
2023-09-25 22:54:55 +0000

    frees a listint_t list

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,39 +1,38 @@
 #include "lists.h"
 
 /**
- * free_listint_safe - Frees a listint_t linked list safely
- * @h: A pointer to a pointer to the head of the listint_t list
+ * free_listint_safe - Frees a listint_t list safely
+ * @h: A pointer to a pointer to the head of the list
  *
  * Return: The size of the list that was freed
  */
 size_t free_listint_safe(listint_t **h)
 {
 	size_t size = 0;
-	listint_t *next_node;
+	listint_t *current, *tmp;
 
-	next_node = *h;
-	while (next_node != NULL)
+	if (h == NULL || *h == NULL)
+		return (0);
+
+	current = *h;
+
+	while (current != NULL)
 	{
-		next_node = *h;
-		*h = next_node->next;
-		free_listint(next_node);
-		size++;
+		if (current <= (current)->next)
+		{
+			tmp = current->next;
+			free(current);
+			current = tmp;
+			size++;
+		}
+		else
+		{
+			free(current);
+			current = NULL;
+			size++;
+			break;
+		}
 	}
-
+	*h = NULL;
 	return (size);
-}
-
-/**
- * free_listint - Frees a listint_t list
- * @head: a pointer to the head of the listint_t list
- *
- * Return: void
- */
-void free_listint(listint_t *head)
-{
-	if (head == NULL)
-		return;
-
-	free_listint(head->next);
-	free(head);
 }
