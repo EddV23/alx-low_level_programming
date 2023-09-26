df75842f539c4547584343e92afce756d5bcce82
EddV23 <edjevan40@gmail.com>
2023-09-26 00:18:34 +0000

    frees a listint_t list

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,41 +1,48 @@
 #include "lists.h"
 
+void free_listint(listint_t *head);
+
 /**
- * free_listint_safe - Frees a listint_t list safely
- * @h: A pointer to a pointer to the head of the list
+ * free_listint_safe - Frees a listint_t linked list safely
+ * @h: A pointer to a pointer to the head of the listint_t list
  *
  * Return: The size of the list that was freed
  */
 size_t free_listint_safe(listint_t **h)
 {
 	size_t size = 0;
-	listint_t *current, *tmp;
-	int distance;
+	listint_t *current, *next_node;
 
 	if (h == NULL || *h == NULL)
 		return (0);
 
 	current = *h;
-
-	while (current != NULL)
+	while (next_node != NULL)
 	{
-		distance = current - current->next;
-
-		if (distance > 0)
-		{
-			tmp = current->next;
-			free(current);
-			current = tmp;
-			size++;
-		}
-		else
-		{
-			free(current);
-			current = NULL;
-			size++;
-			break;
-		}
+		next_node = current->next;
+		free(current);
+		current = next_node;
+		size++;
 	}
+
 	*h = NULL;
 	return (size);
+}
+
+/**
+ * free_listint - Frees a listint_t list
+ * @head: a pointer to the head of the listint_t list
+ *
+ * Return: void
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *next_node;
+
+	while (head != NULL)
+	{
+		next_node = head;
+		next_node = next_node->next;
+		free(next_node);
+	}
 }
