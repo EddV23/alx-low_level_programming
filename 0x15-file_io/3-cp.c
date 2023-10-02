09dc068d9f9eca8f965ec984deb1b673fa5b22c7
EddV23 <edjevan40@gmail.com>
2023-10-02 19:00:10 +0000

    copies the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,13 +16,13 @@ int copy_file(int from, int to)
 	{
 		bytes_written = write(to, buffer, bytes_read);
 		if (bytes_written == -1)
-			exit(99);
+			return (99);
 	}
 
 	if (bytes_read == -1)
-		exit(98);
+		return (98);
 
-	exit(0);
+	return (0);
 }
 
 /**
@@ -39,14 +39,14 @@ int main(int ac, char **av)
 	if (ac != 3)
 	{
 		dprintf(2, "Usage: %s file_from file_to\n", av[0]);
-		exit(97);
+		return (97);
 	}
 
 	fd_from = open(av[1], O_RDONLY);
 	if (fd_from == -1)
 	{
 		dprintf(2, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
+		return (98);
 	}
 
 	fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
@@ -54,21 +54,21 @@ int main(int ac, char **av)
 	{
 		dprintf(2, "Error: Can't write to %s\n", av[2]);
 		close(fd_from);
-		exit(99);
+		return (99);
 	}
 
 	if (copy_file(fd_from, fd_to) != 0)
 	{
 		close(fd_from);
 		close(fd_to);
-		exit(99);
+		return (99);
 	}
 
 	if (close(fd_from) == -1 || close(fd_to) == -1)
 	{
 		dprintf(2, "Error: Can't close fd %d\n", fd_from == -1 ? fd_to : fd_from);
-		exit(100);
+		return (100);
 	}
 
-	exit(0);
+	return (0);
 }
