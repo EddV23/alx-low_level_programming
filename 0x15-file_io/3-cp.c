29087d763b121c19bb917755f60e3987b802372f
EddV23 <edjevan40@gmail.com>
2023-10-02 18:32:01 +0000

    copies the content of a file to another file.

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,74 @@
+#include "main.h"
+
+/**
+ * copy_file - copy the content of one file to another
+ * @from: Source file descriptor
+ * @to: Destination file descriptor
+ *
+ * Return: 0 on success, or the corresponding error code on failure
+ */
+int copy_file(int from, int to)
+{
+	ssize_t bytes_read, bytes_written;
+	char buffer[1024];
+
+	while ((bytes_read = read(from, buffer, sizeof(buffer))) > 0)
+	{
+		bytes_written = write(to, buffer, bytes_read);
+		if (bytes_written == -1)
+			return (99);
+	}
+
+	if (bytes_read == -1)
+		return (98);
+
+	return (0);
+}
+
+/**
+ * main - Entry point, copy the content of one file to another file
+ * @argc: Argument count
+ * @argv: Argument vector
+ *
+ * Return: 0 on success, or the corresponding error code on failure
+ */
+int main(int argc, char **argv)
+{
+	int fd_from, fd_to;
+
+	if (argc != 3)
+	{
+		dprintf(2, "Usage: %s file_from file_to\n", argv[0]);
+		return (97);
+	}
+
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		return (98);
+	}
+
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_to == -1)
+	{
+		dprintf(2, "Error: Can't write to %s\n", argv[2]);
+		close(fd_from);
+		return (99);
+	}
+
+	if (copy_file(fd_from, fd_to) != 0)
+	{
+		close(fd_from);
+		close(fd_to);
+		return (99);
+	}
+
+	if (close(fd_from) == -1 || close(fd_to) == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", fd_from == -1 ? fd_to : fd_from);
+		return (100);
+	}
+
+	return (0);
+}
