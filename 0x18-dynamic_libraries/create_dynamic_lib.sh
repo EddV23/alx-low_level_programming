9c03abdfea701464c58571617313c619b95f7c5d
EddV23 <edjevan40@gmail.com>
2023-11-14 07:10:01 +0000

    deleted some files

--- 0x18-dynamic_libraries/create_dynamic_lib.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-gcc -Wall -fPIC -pedantic -Werror -Wextra -c *.c
-gcc -shared -o libdynamic.so *.o
--- 0x18-dynamic_libraries/myrand.c
+++
@@ -1,7 +0,0 @@
-#include <stdlib.h>
-
-int rand(void)
-{
-	/*Override rand() to always return 6*/
-	return (6);
-}
